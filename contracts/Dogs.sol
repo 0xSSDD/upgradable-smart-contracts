29e07f9f7143cb5814e94c42b0790412e122f905
SSDD <0xSSDD@protonmail.com>
2022-06-05 19:16:29 +0300

    adds Storage contract and incorrect Dog contract

---
+++ contracts/Dogs.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.8.14;
+
+import "./Storage.sol";
+
+contract Dog is Storage {
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function getNumberOfDogs() public view returns(uint256) {
+    return _uintStorage["Dogs"];
+  }
+
+  function setNumberOfDogs(uint256 _dogNumber) public  {
+     _uintStorage["Dogs"] = _dogNumber;
+  }
+
+}
